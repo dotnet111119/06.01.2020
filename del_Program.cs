e7e0b9fadc10025c4fbc18c81df37ec2c1e5e666
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2020-01-06 20:02:15 +0200

    Add files via upload

---
+++ del_Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3012T
+{
+
+    class Program
+    {
+
+        delegate void Method_Signature_return_void_accepts_int(int x);
+
+        static void Do1(int  x)
+        {
+            Console.WriteLine($"i got {x}");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static void Do2(int x)
+        {
+            Console.WriteLine($"i got {x}");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Method_Signature_return_void_accepts_int del1 = Do1;
+            
+            //Do1(5);
+            //del1.Invoke(5); // same as Do1(5)
+            del1.BeginInvoke(5, null, null);
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ del_Program_adv.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3012T
+{
+
+    class Program
+    {
+
+        delegate double Method_return_double_accepts_2_doubles(double a, double b);
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        static double Div(double a, double b)
+        {
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Number 1:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Number 2:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Operation + - * / ");
+            string operation = Console.ReadLine();
+
+            Dictionary<string, Method_return_double_accepts_2_doubles> mapOperToMethod = 
+                new Dictionary<string, Method_return_double_accepts_2_doubles>();
+            mapOperToMethod.Add("+", Add);
+            mapOperToMethod.Add("ADD", Add);
+            mapOperToMethod.Add("-", Minus);
+            mapOperToMethod.Add("*", Mul);
+            mapOperToMethod.Add("/", Div);
+
+            Console.WriteLine(mapOperToMethod[operation].Invoke(x, y));
+        }
+    }
+}
